5e9f938573d1dbf5b2bfbe2cd1ff962e72501ffe
Phoebe Lau <phoebelau7@gmail.com>
2018-01-03 22:18:22 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,8 +1,5 @@
-+class ChangeDatatypeForBirthdate < ActiveRecord::Migration
- +
- +
- +  def change
- +    change_column :students, :birthdate, :datetime
- +  end
- +
- +end
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration
+  def change
+    change_column :students, :birthdate, :datetime
+  end
+end
